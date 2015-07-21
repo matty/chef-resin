48074a9c1c25d2599cbdd345ac545b9ca95a1285
Daniel Jimenez <daniel.j.jimenez@gmail.com>
2015-07-21 15:11:31 -0500

    Lower postgres version.

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,4 +4,4 @@ default[:resin][:home] = File.join(Dir.home(node[:resin][:user]), 'resin')
 default[:resin][:base_dir] = File.expand_path("..", node[:resin][:home])
 default[:resin][:version] = '4.0.41'
 default[:resin][:sqlitejdbc][:version] = '3.8.5-pre1'
-default[:resin][:postgresql][:version] = '9.4-1201-jdbc41'
+default[:resin][:postgresql][:version] = '9.3-1102-jdbc41'
